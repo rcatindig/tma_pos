cccbca05218805bdc26fc5f91b8c4f8c1e347a7e
Reynard Catindig <Reynard@DWEXT70.tmagroup.local>
2018-03-22 16:09:02 +0800

    fix

--- tma_admin/src/App.js
+++ tma_admin/src/App.js
@@ -18,11 +18,11 @@ class App extends Component {
 
 		<HashRouter>
 			<Switch>
-			{/* <Route exact path="/" name="Login Page" component={Login}/>
-			<Route exact path="/register" name="Register Page" component={Register}/>
-			<Route exact path="/404" name="Page 404" component={Page404}/>
-			<Route exact path="/500" name="Page 500" component={Page500}/> */}
-			<Route path="/" name="Dashboard" component={Main}/>
+				{/* <Route exact path="/" name="Login Page" component={Login}/>
+				<Route exact path="/register" name="Register Page" component={Register}/>
+				<Route exact path="/404" name="Page 404" component={Page404}/>
+				<Route exact path="/500" name="Page 500" component={Page500}/> */}
+				<Route path="/" name="Dashboard" component={Main}/>
 			</Switch>
 		</HashRouter>
       
