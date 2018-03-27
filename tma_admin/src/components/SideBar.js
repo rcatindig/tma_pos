1fb55e0fc3980e8b85975b093db95f936cb0dfaf
Reynard Catindig <Reynard@DWEXT70.tmagroup.local>
2018-03-27 10:09:35 +0800

    fix sidebar

--- tma_admin/src/components/SideBar.js
+++ tma_admin/src/components/SideBar.js
@@ -15,14 +15,16 @@ class SideBar extends Component {
                             <li><Link to="/redirect"><i className="fa fa-tachometer"></i>Dashboard</Link></li>
                             <li className="nav-label">Main</li>
                             <li><Link to="/redirect/transactions"><i className="fa fa-car"></i>Transactions</Link></li>
+                            <li><Link to="/"><i className="fa fa-cogs"></i>Machines</Link></li>
                             <li> <a className="has-arrow  " onClick={() => console.log('collapse') } aria-expanded="false"><i className="fa fa-wpforms"></i><span className="hide-menu">Reports</span></a>
                                 <ul aria-expanded="false" className="collapse">
-                                    <li><a href="uc-calender.html">Weekly Report</a></li>
-                                    <li><a href="uc-datamap.html">Monthly Report</a></li>
-                                    <li><a href="uc-nestedable.html">Yearly Report</a></li>
+                                    <li><a href="uc-calender.html">Transaction Report</a></li>
+                                    <li><a href="uc-datamap.html">Entry Report</a></li>
+                                    <li><a href="uc-nestedable.html">Exit Report</a></li>
                                 </ul>
                             </li>
                             <li className="nav-label">Setup</li>
+                            <li><Link to="/"><i className="fa fa-industry"></i>Companies</Link></li>
                             <li><Link to="/"><i className="fa fa-user-circle"></i>Users</Link></li>
                             
                         </ul>
