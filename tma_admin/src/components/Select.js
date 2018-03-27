0ba6fa1dcfbc6044bfcf0830ee25326b33738a6a
Reynard Catindig <Reynard@DWEXT70.tmagroup.local>
2018-03-27 15:41:48 +0800

    create Select component

---
+++ tma_admin/src/components/Select.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Select extends Component {
+
+    static defaultProps = {
+        className: "",
+        id:"",
+        options: [],
+        name: ""
+    };
+  
+    constructor(props) {
+        super(props);
+    }
+
+    
+    render() {
+        const { id, className, options, name } = this.props;
+        console.log(options);
+        return (
+            <select className={className} id={id} name={name}>
+                <option>Please select...</option>
+                {options.map(function(a) {
+                    return (
+                        <option value={a.value}>{a.label}</option>
+                    );
+                })}
+            </select>
+        );
+    }
+
+}
+
+export { Select };
